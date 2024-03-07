feb82d906ea6e1b35b307d29723c0a1af5242119
cnobleshriver <100378265+cnobleshriver@users.noreply.github.com>
2024-03-07 16:01:46 -0500

    first commit

---
+++ src/script.js
@@ -0,0 +1,27 @@
+const printMe = (i, j) => {
+  console.log("You clicked on (" + (i + 1) + ", " + (j + 1) + ")");
+};
+
+const build = () => {
+  const numCols = 8,
+    numRows = 8,
+    pieces = [
+      "rook",
+      "knight",
+      "bishop",
+      "queen",
+      "king",
+      "bishop",
+      "knight",
+      "rook",
+    ],
+    theGrid = document.getElementById("theGrid");
+
+  // TASK #2: Build The Chessboard Using JavaScript
+  //
+  // Build the chessboard with clicking application behavior
+  // Add your implemention here.
+  
+};
+
+build();
